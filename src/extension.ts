8cb7b6463568413f3e8a1034d7171bbaec573fdb
Daniel Priebe <daniel.priebe@cronn.de>
2020-10-24 19:25:57 +0200

    Use DocumentFormattingEditProvider instead of DocumentRangeFormattingEditProvider

--- src/extension.ts
+++ src/extension.ts
@@ -2,48 +2,61 @@ import * as vscode from 'vscode';
 import { execSync } from 'child_process';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.languages.registerDocumentRangeFormattingEditProvider(
-		{ scheme: 'file', language: 'java' }, {
-		provideDocumentRangeFormattingEdits(
-			document: vscode.TextDocument,
-			range: vscode.Range,
-			options: vscode.FormattingOptions,
-			token: vscode.CancellationToken): Promise<vscode.TextEdit[]> {
-			if (range.isEmpty) {
-				return Promise.resolve([]);
-			}
-
-			return runFormatter(document.getText(range)).then(
-				stdout => {
-					return Promise.resolve([
-						vscode.TextEdit.replace(range, stdout)
-					]);
-				},
-				reason => {
-					return Promise.reject(reason);
-				}
-			);
-		}
-	});
+	let disposable = vscode.languages.registerDocumentFormattingEditProvider(
+		{ scheme: 'file', language: 'java' },
+		new DocumentFormatter()
+	);
 
 	context.subscriptions.push(disposable);
 }
 
 export function deactivate() { }
 
-function runFormatter(textRange: string): Promise<string> {
-	return new Promise((resolve, reject) => {
-		try {
-			let stdout: string = execSync(
+abstract class AbstractFormatter {
+	private run(text: string): Promise<string> {
+		return new Promise((resolve, reject) => {
+			const stdout = execSync(
 				`java -jar ${vscode.workspace.getConfiguration('gjf').get('jarPath')} -`,
 				{
-					encoding: "utf8",
-					input: textRange,
+					input: text,
 					windowsHide: true
-				});
+				})
+				.toString();
+
 			resolve(stdout);
-		} catch (e) {
-			reject(e);
-		}
-	});
+		});
+	}
+
+	protected format(document: vscode.TextDocument,
+		range: vscode.Range,
+		options: vscode.FormattingOptions,
+		token: vscode.CancellationToken): Promise<vscode.TextEdit[]> {
+		return new Promise((resolve, reject) => {
+			token.onCancellationRequested(e => reject(e));
+
+			if (range.isEmpty) {
+				resolve([]);
+			}
+
+			this.run(document.getText(range))
+				.then(s => resolve([vscode.TextEdit.replace(range, s)]))
+				.catch(e => reject(e));
+		});
+	}
+}
+
+class DocumentFormatter extends AbstractFormatter implements vscode.DocumentFormattingEditProvider {
+	provideDocumentFormattingEdits(
+		document: vscode.TextDocument,
+		options: vscode.FormattingOptions,
+		token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
+		return this.format(
+			document,
+			new vscode.Range(
+				document.lineAt(0).range.start,
+				document.lineAt(document.lineCount - 1).range.end
+			),
+			options,
+			token);
+	}
 }
