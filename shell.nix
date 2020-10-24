010de2681b2d195eeabdf55053d86e812bc1b113
Daniel Priebe <daniel.priebe@cronn.de>
2020-10-24 19:18:16 +0200

    Update shell.nix + tasks.json

--- shell.nix
+++ shell.nix
@@ -1,3 +1,12 @@
-{ pkgs ? import <nixpkgs> { } }:
-with pkgs;
-mkShell { buildInputs = [ nodejs-slim-14_x yarn ]; }
+{ pkgs ? import <nixpkgs> {} }:
+with (pkgs);
+let
+  nixPackages = [ rnix-lsp nixpkgs-fmt ];
+  jsPackages = with nodePackages; [ nodejs-slim-14_x yarn eslint ];
+in
+mkShell {
+  buildInputs = nixPackages ++ jsPackages;
+  shellHook = ''
+    export PATH="$PWD/node_modules/.bin/:$PATH"
+  '';
+}
